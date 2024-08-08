00a35fb33ffdd5eb4d7192d8d27943f5933ffda9
SultanBajaifer <realsultanbajaifer@gmail.com>
2024-08-08 03:01:39 +0300

    init the project, need to now work on authentication, layout, routing, state, and more

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello and Welcome to OMS</span>
+    </div>
+  )
+}
+
+export default App
