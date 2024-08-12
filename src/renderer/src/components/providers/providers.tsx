42a201d81ff89970f08c6d9679b09b4bbc37ed52
Hamza Omar Balala <103255525+HamzaBlala49@users.noreply.github.com>
2024-08-12 20:15:11 +0300

    login page and some fix some error

--- src/renderer/src/components/providers/providers.tsx
+++ src/renderer/src/components/providers/providers.tsx
@@ -1,6 +1,4 @@
-import { DirectionProvider } from '@radix-ui/react-direction'
-import reactAuthStore from '@renderer/lib/react-auth-store'
-import AuthProvider from 'react-auth-kit'
+import { AuthProvider } from 'react-auth-kit'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '../router/router'
 import { ReactQueryClientProvider } from './ReactQueryClientProvider'
@@ -8,11 +6,14 @@ import { ReactQueryClientProvider } from './ReactQueryClientProvider'
 const Providers = () => {
   return (
     <ReactQueryClientProvider>
-      <DirectionProvider dir="rtl">
-        <AuthProvider store={reactAuthStore}>
-          <RouterProvider router={router} />
-        </AuthProvider>
-      </DirectionProvider>
+      <AuthProvider
+        authType={'cookie'}
+        authName={'_auth'}
+        cookieDomain={window.location.hostname}
+        cookieSecure={window.location.protocol === 'https:'}
+      >
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ReactQueryClientProvider>
   )
 }
--- src/renderer/src/lib/http.ts
+++ src/renderer/src/lib/http.ts
@@ -1,41 +1,48 @@
 import axios, { AxiosRequestConfig } from 'axios'
-import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated'
-import { redirect } from 'react-router-dom'
+// import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated'
+// import { redirect } from 'react-router-dom'
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'http://mjeedsalem12-001-site1.htempurl.com',
   auth: {
-    username: process.env.REACT_APP_API_USERNAME as string,
-    password: process.env.REACT_APP_API_PASSWORD as string
+    username: '11189255',
+    password: '60-dayfreetrial'
+  },
+  headers: {
+    'Access-Control-Allow-Origin': true
   }
 })
 
 type Config = AxiosRequestConfig<any> | undefined
 
-function isAuth() {
-  const isAuthenticated = useIsAuthenticated()
-  if (isAuthenticated) return true
+// function isAuth() {
+//   const isAuthenticated = useIsAuthenticated()
+//   console.log(isAuthenticated)
+//   if (isAuthenticated) return true
 
-  redirect('login')
-  return false
-}
+//   redirect('login')
+//   return false
+// }
 
 export function getApi<T>(url: string, config?: Config) {
-  if (isAuth()) return axiosInstance.get<T>(url, config)
+  // if (isAuth()) return
+  return axiosInstance.get<T>(url, config)
 }
 
-export function putApi<T>(url: string, data: any, config?: Config) {
-  if (isAuth()) return axiosInstance.put<T>(url, data, config)
-}
+// export function putApi<T>(url: string, data: any, config?: Config) {
+//   if (isAuth()) return axiosInstance.put<T>(url, data, config)
+// }
 
 export function postApi<T>(url: string, data: any, config?: Config) {
-  if (isAuth()) return axiosInstance.post<T>(url, data, config)
+  // if (isAuth()) return
+  console.log(url)
+  return axiosInstance.post<T>(url, data, config)
 }
 
-export function patchApi<T>(url: string, data: any, config?: Config) {
-  if (isAuth()) return axiosInstance.patch<T>(url, data, config)
-}
+// export function patchApi<T>(url: string, data: any, config?: Config) {
+//   if (isAuth()) return axiosInstance.patch<T>(url, data, config)
+// }
 
-export function deleteApi<T>(url: string, config?: Config) {
-  if (isAuth()) return axiosInstance.delete<T>(url, config)
-}
+// export function deleteApi<T>(url: string, config?: Config) {
+//   if (isAuth()) return axiosInstance.delete<T>(url, config)
+// }
