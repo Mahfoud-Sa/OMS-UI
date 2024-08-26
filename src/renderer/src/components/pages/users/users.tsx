e2ba9e023c5156eff33cd105c8db65723f7518f6
Hamza Omar Balala <103255525+HamzaBlala49@users.noreply.github.com>
2024-08-26 19:03:44 +0300

    refactor user management
    Add some modification in home page of users ,  user add page and  user info-edit page

--- src/renderer/src/components/pages/users/users.tsx
+++ src/renderer/src/components/pages/users/users.tsx
@@ -3,90 +3,59 @@ import { Skeleton } from '@renderer/components/ui/skeleton'
 import { useToast } from '@renderer/components/ui/use-toast'
 import { UserCard } from '@renderer/components/ui/UserCard'
 import { deleteApi, getApi } from '@renderer/lib/http'
+import { DeliveryUserCardProps } from '@renderer/types'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
 import Statistics from './_components/statistics'
 import UsersSearch from './_components/users-search'
 
 const Users = () => {
-  interface DeliveryUserCardProps {
-    id: string
-    fullName: string
-    userName: string
-    userType: string
-    imagePath?: string
-    phone: string
-    workPlace: string
-  }
+  const [usersData, setUsersData] = useState<DeliveryUserCardProps[] | undefined>(undefined)
+  const [selectedRole, setSelectedRole] = useState<string | undefined>(undefined)
+  const { toast } = useToast()
 
-  // Add more user data as needed
+  // get users
   const {
     data: fetchedData,
-    isLoading,
+    isPending,
+    isError,
     error
   } = useQuery({
     queryKey: ['users'],
     queryFn: () =>
       getApi<{ users: DeliveryUserCardProps[] }>('/api/Users', { params: { page: 1, pageSize: 2 } })
   })
+
   useEffect(() => {
     if (fetchedData?.data.users) {
       setUsersData(fetchedData.data.users)
     }
   }, [fetchedData])
 
-  console.log(fetchedData)
-
-  const [usersData, setUsersData] = useState<DeliveryUserCardProps[] | undefined>(undefined)
-  console.log(usersData)
-  const [selectedRole, setSelectedRole] = useState<string | undefined>(undefined)
-  const { toast } = useToast()
+  // delete users
   const queryClient = useQueryClient()
-  const mutation = useMutation({
+  const { mutate } = useMutation({
     mutationFn: (id: string) => {
       return deleteApi(`/api/Users/${id}`)
+    },
+    onSuccess: () => {
+      toast({
+        title: 'تم الحذف بنجاح',
+        description: `تم إتمام العملية بنجاح`,
+        variant: 'success'
+      })
+      queryClient.invalidateQueries({ queryKey: ['users'] })
+    },
+    onError: () => {
+      toast({
+        title: 'فشلت علمية الحذف',
+        description: `حصل خطأ ما`,
+        variant: 'destructive'
+      })
     }
-    // onMutate: async (id) => {
-    //   // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-    //   await queryClient.cancelQueries(['users'])
-    //   // Snapshot the previous value
-    //   const previousUsers = queryClient.getQueryData('Users')
-    //   // Optimistically update to the new value
-    //   queryClient.setQueryData('Users', (old) => {
-    //     return old.filter((user) => user.id !== id)
-    //   })
-    //   // Return a context object with the snapshotted value
-    //   return { previousUsers }
-    // },
-    // onError: (err, variables, context) => {
-    //   // If the mutation failed, use the context value to roll back
-    //   queryClient.setQueryData('users', context.previousUsers)
-    // },
-    // onSettled: () => {
-    //   queryClient.invalidateQueries('users')
-    // }
   })
 
-  const removeSelectedUser = async (id: string): Promise<void> => {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        // request to delete user by id
-        mutation.mutate(id)
-        const deletedUserData = usersData?.find((user) => user.id === id)
-        const updateUsersData = usersData?.filter((user) => user.id !== id)
-        setUsersData(updateUsersData)
-        toast({
-          title: 'تم الحذف بنجاح',
-          description: `تم حذف ${deletedUserData?.fullName} بنجاح`,
-          variant: 'success'
-        })
-
-        resolve()
-      } catch (error) {
-        reject(error)
-      }
-    })
-  }
+  // filter users
   const filterUsersByRole = (role: string | undefined) => {
     if (!role) {
       setUsersData(usersData)
@@ -97,6 +66,7 @@ const Users = () => {
     setUsersData(updateUsersData)
     setSelectedRole(role)
   }
+
   return (
     <section className="p-5">
       <Statistics selectedRole={selectedRole} filterData={(role) => filterUsersByRole(role)} />
@@ -105,8 +75,10 @@ const Users = () => {
           <UsersSearch />
           <CreateBtn title={'إضافة مستخدم'} href={'new'} className="w-[200px]" />
         </div>
+
         <div className="p-4 h-96 overflow-auto mt-4">
-          {isLoading && <Skeleton className="h-96" />}
+          {isPending && <Skeleton className="h-96" />}
+          {isError && <div>{error.message}</div>}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {usersData?.map((user) => (
               <UserCard
@@ -118,7 +90,7 @@ const Users = () => {
                   phone: user.phone,
                   workPlace: user.workPlace
                 }}
-                removeSelectedUser={(id) => removeSelectedUser(id)}
+                removeSelectedUser={(id) => mutate(id)}
               />
             ))}
           </div>
