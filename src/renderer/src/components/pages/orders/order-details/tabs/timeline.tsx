c062df8592b672777bc2dfa084dc839fad76338b
Hamza Omar Balala <103255525+HamzaBlala49@users.noreply.github.com>
2024-10-28 13:07:17 -0700

    create timeline action buttons

--- src/renderer/src/components/pages/orders/order-details/tabs/timeline.tsx
+++ src/renderer/src/components/pages/orders/order-details/tabs/timeline.tsx
@@ -1,5 +1,25 @@
+import { Button } from '@renderer/components/ui/button'
+import { LucideHand, Printer, X } from 'lucide-react'
+
 const Timeline = () => {
-  return <div>Timeline</div>
+  return (
+    <section>
+      <div className="flex gap-2 justify-end">
+        <Button className="flex bg-red-600 hover:bg-red-700  gap-2 ">
+          الغاء الطلبية
+          <X />
+        </Button>
+        <Button className="flex  gap-2 bg-green-600 hover:bg-green-700 ">
+          إنتهاء التصنيع
+          <LucideHand />
+        </Button>
+        <Button className="flex gap-2">
+          طباعة
+          <Printer />
+        </Button>
+      </div>
+    </section>
+  )
 }
 
 export default Timeline
