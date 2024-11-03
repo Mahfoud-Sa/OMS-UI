722a3ee8e92a039d37e177c795adfc354881f83c
SultanBajaifer <realsultanbajaifer@gmail.com>
2024-11-04 00:54:38 +0300

    Refactor timeline component and add localization for Arabic language

--- src/renderer/src/components/pages/orders/order-details/tabs/timeline.tsx
+++ src/renderer/src/components/pages/orders/order-details/tabs/timeline.tsx
@@ -6,12 +6,15 @@ import { Item } from '@renderer/types/api'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { Check, LucideHand, Printer, X } from 'lucide-react'
 import moment from 'moment'
+import 'moment/dist/locale/ar-ma'
 import { useNavigate, useParams } from 'react-router-dom'
 import AddTimeLineDialog from '../_components/AddTimeLineDialog'
 
 const Timeline = () => {
   const { id } = useParams()
   const navigate = useNavigate()
+
+  moment.locale('ar-ma')
 
   const { data, isPending, error, isError } = useQuery({
     queryKey: ['time_line', id],
@@ -29,7 +32,7 @@ const Timeline = () => {
       })
       navigate('/orders')
     },
-    onError: (error: any) => {
+    onError: () => {
       toast({
         variant: 'destructive',
         title: 'فشلت العملية',
@@ -39,16 +42,16 @@ const Timeline = () => {
   })
   const { mutate: completeOrderMutate, isPending: completeOrderIsPending } = useMutation({
     mutationFn: async () => {
-      await patchApi(`/Orders/${id}`, { orderState: 4 })
+      await patchApi(`/Orders/${id}`, { orderState: 3 })
     },
     onSuccess: () => {
       toast({
         variant: 'success',
-        title: `تم الغاء الطلب بنجاح`
+        title: `تم تسليم الطلب بنجاح`
       })
       navigate('/orders')
     },
-    onError: (error: any) => {
+    onError: () => {
       toast({
         variant: 'destructive',
         title: 'فشلت العملية',
@@ -106,13 +109,13 @@ const Timeline = () => {
       {data.data.map((item, index) => (
         <div key={index} className="my-2 shadow-sm border rounded-sm p-3">
           <div className="flex justify-between items-center">
-            <h1 className="font-bold test-lg">أسم المنتج: {item.fabric}</h1>
+            <h1 className="font-bold test-lg">أسم المنتج: {item.name || item.fabric}</h1>
             <AddTimeLineDialog id={item.id.toString()} />
           </div>
-          <h1 className="font-medium test-sm">أسم المصنع: {item.id}</h1>
+          <h1 className="font-medium test-sm">أسم المصنع: {item.factoryName || item.id}</h1>
           <div className="flex gap-3 flex-wrap my-2 shadow-sm border px-3 pt-3  rounded-sm">
             {item.timelines.map((timeline, index) => (
-              <>
+              <div key={`${index}`}>
                 {(timeline.status == 0 || timeline.status == 2) && (
                   <div className="flex gap-2 border-b-2 border-primary pb-2">
                     <div className=" font-bold flex justify-center items-center border-2 border-primary h-[45px] w-[45px] rounded-full text-primary">
@@ -123,7 +126,7 @@ const Timeline = () => {
                         {timeline.productionLineName}
                       </h3>
                       <p className="text-[#ABB7C2] text-sm">
-                        {new Date(timeline.receivedAt).toLocaleDateString()}
+                        بدء في {new Date(timeline.receivedAt).toLocaleDateString()}
                       </p>
                     </div>
                   </div>
@@ -137,14 +140,16 @@ const Timeline = () => {
                     <div className="flex flex-col gap-1">
                       <h3 className="text-base font-medium">{timeline.productionLineName}</h3>
                       <p className="text-[#ABB7C2] text-sm">
-                        {moment(new Date(timeline.deliveredAt))
-                          .locale('ar')
-                          .from(moment(new Date(timeline.receivedAt), true).locale('ar'))}
+                        {`${'انتهت خلال'}
+                        ${moment(new Date(timeline.receivedAt)).from(
+                          moment(new Date(timeline.deliveredAt)),
+                          true
+                        )}`}
                       </p>
                     </div>
                   </div>
                 )}
-              </>
+              </div>
             ))}
           </div>
         </div>
