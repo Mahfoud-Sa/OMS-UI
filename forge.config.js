ac145cbb84544c6828e2ac97020ed4a2a0834c23
SultanBajaifer <realsultanbajaifer@gmail.com>
2024-11-12 15:56:51 +0300

    Refactor release workflow to improve environment variable handling and update app publishing process

--- forge.config.js
+++ forge.config.js
@@ -11,7 +11,7 @@ module.exports = {
           name: 'OMS-UI'
         },
         prerelease: true,
-        authToken: import.meta.env.GITHUB_TOKEN,
+        authToken: process.env.GITHUB_TOKEN,
         force: true
       }
     }
