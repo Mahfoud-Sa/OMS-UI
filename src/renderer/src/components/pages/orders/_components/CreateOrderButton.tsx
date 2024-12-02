61429e92fc242831ffa4518339d9c3575a6b66ba
SultanBajaifer <realsultanbajaifer@gmail.com>
2024-12-02 16:35:14 +0300

    Update release workflow to remove 'dev_' prefix from version tags, bump version to 1.2.1 in package.json, and enhance CreateOrderButton to consider user roles for enabling the button

--- src/renderer/src/components/pages/orders/_components/CreateOrderButton.tsx
+++ src/renderer/src/components/pages/orders/_components/CreateOrderButton.tsx
@@ -3,9 +3,12 @@ import { gotRole } from '@renderer/lib/utils'
 import { Roles } from '@renderer/types/api'
 
 const CreateOrderButton = () => {
+  const userRoles: string[] = localStorage.getItem('_auth_state')
+    ? JSON.parse(localStorage.getItem('_auth_state') || '{}').roles
+    : []
   return (
     <CreateBtn
-      disable={!gotRole(Roles.AddOrder)}
+      disable={!gotRole(Roles.AddOrder) && userRoles.length > 0}
       title={'إضافة طلب'}
       href={'new'}
       className="w-[200px]"
