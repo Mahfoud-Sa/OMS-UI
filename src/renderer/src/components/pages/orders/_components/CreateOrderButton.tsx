a9f7d8f0dd5db4a4507b909681ab26746f5ab4c0
SultanBajaifer <realsultanbajaifer@gmail.com>
2024-12-02 16:03:58 +0300

    Update CreateOrderButton to enable role-based access control for order creation

--- src/renderer/src/components/pages/orders/_components/CreateOrderButton.tsx
+++ src/renderer/src/components/pages/orders/_components/CreateOrderButton.tsx
@@ -1,6 +1,15 @@
 import CreateBtn from '@renderer/components/layouts/create-btn'
+import { gotRole } from '@renderer/lib/utils'
+import { Roles } from '@renderer/types/api'
 
 const CreateOrderButton = () => {
-  return <CreateBtn disable={true} title={'إضافة طلب'} href={'new'} className="w-[200px]" />
+  return (
+    <CreateBtn
+      disable={!gotRole(Roles.AddOrder)}
+      title={'إضافة طلب'}
+      href={'new'}
+      className="w-[200px]"
+    />
+  )
 }
 export default CreateOrderButton
