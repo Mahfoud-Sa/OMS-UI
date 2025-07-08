2c89e347b157a9244130e53bd092414f4a572066
SultanBajaifer <realsultanbajaifer@gmail.com>
2025-07-09 02:17:48 +0300

    Refactor sourcemap upload script to inject sourcemaps before uploading; comment out release creation and finalization steps

--- scripts/upload-sourcemaps.js
+++ scripts/upload-sourcemaps.js
@@ -5,8 +5,12 @@ const release = `${pkg.version}`
 
 console.log(`Uploading Sentry sourcemaps for release: ${release}`)
 
-execSync(`sentry-cli releases new ${release}`, { stdio: 'inherit' })
-execSync(`sentry-cli sourcemaps upload --org herfa --project electron ./out --release ${release}`, {
-  stdio: 'inherit'
-})
-execSync(`sentry-cli releases finalize ${release}`, { stdio: 'inherit' })
+// execSync(`sentry-cli releases new ${release}`, { stdio: 'inherit' })
+execSync(
+  `sentry-cli sourcemaps inject --org herfa --project electron ./out && sentry-cli sourcemaps upload --org herfa --project electron ./out --release ${release}`,
+  {
+    stdio: 'inherit'
+  }
+)
+
+// execSync(`sentry-cli releases finalize ${release}`, { stdio: 'inherit' })
